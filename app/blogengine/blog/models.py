33c7f7c162a43935d660b178f679e8f2a0c06ee5
Nikita <nikita@MacBook-Air-Nikita.local>
2021-12-29 20:41:23 +0300

    добавлены теги, изменен шаблон карточки

--- app/blogengine/blog/models.py
+++ app/blogengine/blog/models.py
@@ -7,6 +7,7 @@ class Post(models.Model):
     slug = models.SlugField(max_length=150, unique=True)
     body = models.TextField(blank=True, db_index=True)
     data_pub = models.DateTimeField(auto_now_add=True)
+    tags = models.ManyToManyField('Tag', blank=True, related_name='posts')
 
     def get_absolute_url(self):
         return reverse('post_detail_url', kwargs={'slug': self.slug})
@@ -14,3 +15,13 @@ class Post(models.Model):
     def __str__(self):
         return f'{self.title}'
 
+
+class Tag(models.Model):
+    title = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=50, unique=True)
+
+    def __str__(self):
+        return f'{self.title}'
+    
+    def get_absolute_url(self):
+        return reverse('tag_detail_url', kwargs={'slug': self.slug})
